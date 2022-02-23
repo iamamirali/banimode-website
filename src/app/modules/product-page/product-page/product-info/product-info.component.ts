f51f1115729e14dd92d94172a24c079cea2ff372
iamamirali <iamamirali.sh@gmail.com>
2022-02-23 22:58:05 +0330

    add other colors style in product-page

--- src/app/modules/product-page/product-page/product-info/product-info.component.ts
+++ src/app/modules/product-page/product-page/product-info/product-info.component.ts
@@ -10,6 +10,17 @@ import { SugessDatum } from 'src/app/models/suggestions.model';
 })
 export class ProductInfoComponent implements OnInit {
 
+  colorSlideConfig = {
+    "slidesToShow": 6,
+    "slidesToScroll": 6,
+    "arrows": true,
+    "prevArrow": '<div style=\'position: absolute; top: -38px; left: 22%; z-index: 10; cursor: pointer; font-size: 1.6rem;\' class=\'next-slide\'><i class="fa fa-angle-right" style=\'color: gray;\'></i></div>',
+    "nextArrow": '<div style=\'position: absolute; top: -38px; left: 0px; z-index: 10; cursor: pointer; font-size: 1.6rem\' class=\'next-slide\'><i class="fa fa-angle-left" style=\'color: gray;\'></i></div>',
+    "infinite": false,
+    "rtl": true,
+    'dots': true,
+  }
+
   selectedColor: string = ''
 
   @Input() productItem: FlashDatum | SugessDatum | undefined
@@ -23,5 +34,7 @@ export class ProductInfoComponent implements OnInit {
   onColorClick(color: IColorImage) {
     this.colorData.emit(color)
     this.selectedColor = color.name
+    console.log(this.selectedColor);
+    
   }
 }
--- src/app/modules/product-page/product-page/product-page.component.ts
+++ src/app/modules/product-page/product-page/product-page.component.ts
@@ -28,14 +28,15 @@ export class ProductPageComponent implements OnInit {
       this.productList = data.data.data
       if(!this.productItem) {
         this.productItem = this.productList.find((item) => item.id_product === +this.route.snapshot.params['id'])
+        this.mainImage = this.productItem ? this.productItem.images.thickbox_default[0] : ''
       }
-      this.mainImage = this.productItem ? this.productItem.images.thickbox_default[0] : ''
       console.log(this.productItem);
     })
     this.dataFetch.getSuggestions().subscribe((data) => {
       this.productList = data.data.data
       if(!this.productItem) {
         this.productItem = this.productList.find((item) => item.id_product === +this.route.snapshot.params['id'])
+        this.mainImage = this.productItem ? this.productItem.images.thickbox_default[0] : ''
       }
     })
   }
@@ -48,5 +49,7 @@ export class ProductPageComponent implements OnInit {
     this.otherColorData = color
     this.mainImage = color.image[0].image_size.thickbox_default
     if(color) this.isInitImg = false
+    console.log('clicked');
+    
   }
 }
